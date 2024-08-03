be0a8e34c3285bfde3f1721f74974a338997b642
Marjona <lalisamarjona17@gmail.com>
2024-08-03 18:57:59 +0500

    12

--- Uzum_Market_Copy/src/main/java/com/example/uzum_market_copy/HelloServlet.java
+++ Uzum_Market_Copy/src/main/java/com/example/uzum_market_copy/HelloServlet.java
@@ -9,7 +9,7 @@ public class HelloServlet extends HttpServlet {
     private String message;
 
     public void init() {
-        message = "Marjona";
+        message = "Hello";
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
