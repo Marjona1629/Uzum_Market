0cc48507ca310a0097f394521c0a7dbbb048f883
Ozoda <ozodaxonzoda05@gmail.com>
2024-08-04 22:48:04 +0500

    db connection

---
+++ Uzum_Market_Copy/src/main/java/uz/pdp/uzummarket/util/DBConnection.java
@@ -0,0 +1,25 @@
+package uz.pdp.uzummarket.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public interface DBConnection {
+
+    String user = "postgres";
+    String password = "1111";
+    String url = "jdbc:postgresql://localhost:5434/uzum_market";
+
+    static Properties getProperties() {
+        Properties properties = new Properties();
+        properties.put("user", user);
+        properties.put("password", password);
+        return properties;
+    }
+    static Connection getConnection() throws SQLException {
+        DriverManager.registerDriver(new org.postgresql.Driver());
+        return  DriverManager.getConnection(url, getProperties());
+    }
+
+}
