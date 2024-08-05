6b64ff219777069d5dad53164b07794c2a80d20c
Marjona <lalisamarjona17@gmail.com>
2024-08-05 16:13:59 +0500

    class

--- Uzum_Market_Copy/src/main/java/uz/pdp/uzummarket/service/UserService.java
+++ Uzum_Market_Copy/src/main/java/uz/pdp/uzummarket/service/UserService.java
@@ -1,4 +1,18 @@
 package uz.pdp.uzummarket.service;
 
 public class UserService {
+
+
+
+    private static UserService userService;
+    public static UserService getInstance(){
+        if (userService == null){
+            synchronized (UserService.class){
+                if (userService == null){
+                    userService = new UserService();
+                }
+            }
+        }
+        return userService;
+    }
 }
